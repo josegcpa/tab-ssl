25f62e9adae4a7f493190b2a154c3dd888390e1e
NMVRodrigues <nmrodrigues@fc.ul.pt>
2022-12-30 15:34:55 +0000

    barebones gpssl commit

--- src/__main__.py
+++ src/__main__.py
@@ -2,7 +2,8 @@ import argparse
 import yaml
 import time
 import json
-import numpy as np 
+import numpy as np
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import ElasticNetCV
 from sklearn.pipeline import Pipeline
@@ -17,9 +18,12 @@ from .data_generator import AutoDataset
 from .data import supported_datasets
 from .decomposition_utilities import supported_decompositions
 
+from .stdgp.StdGP import StdGP
+
 supported_learning_algorithms = {
     "rf":RandomForestClassifier,
-    "linear":ElasticNetCV
+    "linear":ElasticNetCV,
+    "stdgp": StdGP,
 }
 
 if __name__ == "__main__":    
@@ -32,7 +36,7 @@ if __name__ == "__main__":
                         choices=list(supported_decompositions.keys())+["none"],
                         help="Decomposition algorithm ID")
     parser.add_argument("--learning_algorithm",required=True,
-                        choices=["rf","linear"],
+                        choices=["rf","linear", "stdgp"],
                         help="Name of learning algorithm")
     parser.add_argument("--unsupervised_fraction",type=float,
                         help="Fraction of samples to be used for unsupervised\
@@ -86,13 +90,14 @@ if __name__ == "__main__":
             ("dec",decomposition(**decomposition_config)))
 
     pipeline_preprocessing = Pipeline(preproc_transforms)
-    learner = learning_algorithm(
-        **learning_algorithm_config,random_state=args.seed)
-
+    learner = None
+    if args.learning_algorithm != "stdgp" and args.learning_algorithm != "m3gp":
+        learner = learning_algorithm(
+            **learning_algorithm_config,random_state=args.seed)
     cv = ShuffleSplit(args.n_folds,random_state=args.seed)
     splits = cv.split(X)
     
-    def wraper(train_val_idxs):
+    def wraper(train_val_idxs, learner=None):
         train_idxs,val_idxs = train_val_idxs
         train_X = X[train_idxs]
         train_y = y[train_idxs]
@@ -111,15 +116,26 @@ if __name__ == "__main__":
         print("Supervised learning array shape:",
               train_X.shape)
         pipeline_preprocessing.fit(train_X_unsupervised)
-        learner.fit(pipeline_preprocessing.transform(train_X),train_y)
+        if args.learning_algorithm != "stdgp" and args.learning_algorithm != "m3gp":
+            learner.fit(pipeline_preprocessing.transform(train_X),train_y)
+        else:
+            tr_data = pipeline_preprocessing.transform(train_X)
+            tr_data = pd.DataFrame(tr_data, columns=['X'+str(i) for i in range(tr_data.shape[1])])
+            learner = learning_algorithm(**learning_algorithm_config, random_state=args.seed)
+            learner.fit(tr_data, train_y)
         time_b = time.time()
         
         elapsed = time_b - time_a
         
         # export only what is strictly necessary 
         # to compute downstream metrics
-        transformed_X = pipeline_preprocessing.transform(val_X)
-        pred = learner.predict(transformed_X).tolist()
+        if args.learning_algorithm != "stdgp" and args.learning_algorithm != "m3gp":
+            transformed_X = pipeline_preprocessing.transform(val_X)
+            pred = learner.predict(transformed_X).tolist()
+        else:
+            tv_data = pipeline_preprocessing.transform(val_X)
+            tv_data = pd.DataFrame(tv_data, columns=['X'+str(i) for i in range(tv_data.shape[1])])
+            pred = learner.getBestIndividual().predict(tv_data)
         try:
             pred_proba = learner.predict_proba(transformed_X).tolist()
         except:
@@ -140,9 +156,10 @@ if __name__ == "__main__":
     if args.n_workers == 0:
         fold_scoring = []
         for i,(train_idxs,val_idxs) in enumerate(splits):
-            output_dict = wraper((train_idxs,val_idxs))
+            output_dict = wraper((train_idxs,val_idxs), learner)
             fold_scoring.append(output_dict)
-    
+
+    # Needs fix
     else:
         pool = Pool(args.n_workers)
         fold_scoring = pool.map(wraper,splits)
